50fce4a1b2769553b558c1551e8a12acb461ddbc
qjh92@163.com <qjh92@163.com>
2022-05-20 17:51:55 +0800

    开始添加了crontab

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.18
 require (
 	github.com/google/uuid v1.3.0
 	github.com/robfig/cron v1.2.0
+	github.com/robfig/cron/v3 v3.0.1
 )
--- src/crontab/crontab.go
+++ src/crontab/crontab.go
@@ -2,10 +2,32 @@ package crontab
 
 import (
 	"fmt"
-	"github.com/robfig/cron"
+	"github.com/robfig/cron/v3"
 	"log"
 	"os"
+	"time"
 )
+
+/*
+github.com/robfig/cron/v3相关说明
+
+
+ ┌───────────── min (0 - 59)
+ │ ┌────────────── hour (0 - 23)
+ │ │ ┌─────────────── day of month (1 - 31)
+ │ │ │ ┌──────────────── month (1 - 12)
+ │ │ │ │ ┌───────────────── day of week (0 - 6) (0 to 6 are Sunday to
+ │ │ │ │ │                  Saturday)
+ │ │ │ │ │
+ │ │ │ │ │
+ * * * * *
+
+
+*/
+
+
+
+
 
 var mylog *log.Logger
 var fw *os.File
@@ -20,14 +42,26 @@ func Init(a *log.Logger) {
 }
 
 func RunCrontab() {
-	_, b := cron.Parse("* * * * * *")
-	fmt.Println(b)
 
-	//c := cron.New()  // 新建一个定时任务对象
-	//c.AddFunc("* * * * * *", func() {
-	//	log.Println("hello world")
-	//})  // 给对象增加定时任务
-	//c.Start()
-	//select {
-	//}
+	pr:=cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow)
+	_,e:=pr.Parse("*/62 * * * *")
+	fmt.Printf("e=%v\n",e)
+
+	c := cron.New()
+	//a,err:=c.AddFunc("* * * * * *", func() { fmt.Println("Every sec ") })
+	//fmt.Printf("a=%d,err=%v\n",a,err)
+	//c.AddFunc("*/5 * * * * *",      func() { fmt.Println("Every 5 sec") })
+	a,err:=c.AddFunc("* * * * *", func() { fmt.Printf("Every min,%v\n",time.Now()) })
+	fmt.Printf("a=%d,err=%v\n",a,err)
+	a,err=c.AddFunc("*/68 * * * *", func() { fmt.Printf("Every 68 min,%v\n",time.Now()) })
+	fmt.Printf("a=%d,err=%v\n",a,err)
+	a,err=c.AddFunc("*/2 * * * *", func() { fmt.Printf("Every 2 mins,%v\n",time.Now()) })
+	fmt.Printf("a=%d,err=%v\n",a,err)
+	c.Start()
+
+	time.Sleep(65*time.Second)
+	c.Remove(1)
+
+	select {
+	}
 }
